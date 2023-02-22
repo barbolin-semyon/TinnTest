a06b7518fd9807ad402e9e8664e831f2192b2a06
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-22 22:14:09 +0300

    Добавление прозрачного цвета

--- app/src/main/java/com/example/tinntest/ui/theme/Color.kt
+++ app/src/main/java/com/example/tinntest/ui/theme/Color.kt
@@ -4,5 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val LightBlue = Color(0xFFA5D1FF)
 val Blue = Color(0xFF3D88F8)
+val Alpha = Color(0x0000000)
 val Gray = Color(0xFFCBCBCB)
 val DarkGray = Color(0xFF414141)
