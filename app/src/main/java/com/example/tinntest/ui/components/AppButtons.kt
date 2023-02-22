42da9311f779cf6155229467d732dfd9850da7bd
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-22 23:12:57 +0300

    Внесение штрихов в AppButtons.kt

--- app/src/main/java/com/example/tinntest/ui/components/AppButtons.kt
+++ app/src/main/java/com/example/tinntest/ui/components/AppButtons.kt
@@ -1,23 +1,24 @@
 package com.example.tinntest.ui.components
 
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.ButtonElevation
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.tinntest.utils.emailIfValid
 
 @Composable
-fun AppButton(onClick: () -> Unit, enabled: Boolean, text: String) {
+fun AppButton(onClick: () -> Unit, enabled: Boolean, text: String, modifier: Modifier = Modifier) {
     Button(
         onClick = { onClick() },
         enabled = enabled,
-        modifier = Modifier.width(150.dp),
+        colors = ButtonDefaults.buttonColors(
+            disabledBackgroundColor = MaterialTheme.colors.primaryVariant
+        ),
+        modifier = modifier.width(150.dp),
         elevation = ButtonDefaults.elevation(16.dp)
     ) {
-        Text(text)
+        Text(text, color = Color.White)
     }
 }
