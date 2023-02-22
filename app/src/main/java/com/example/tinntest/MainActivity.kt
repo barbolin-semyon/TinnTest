f456e4fd3e447016780887ab0fba68a7fffdd043
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-22 20:42:04 +0300

    Наполнение MainActivity.kt

--- app/src/main/java/com/example/tinntest/MainActivity.kt
+++ app/src/main/java/com/example/tinntest/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.tinntest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.rememberNavController
+import com.example.tinntest.ui.navigation.AppNavHost
+import com.example.tinntest.ui.navigation.Screens
 import com.example.tinntest.ui.theme.TinnTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +14,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TinnTestTheme {
+                val token = LocalContext.current.getSharedPreferences(
+                    "authorization", MODE_PRIVATE
+                ).getString("token", "")
 
+                val isConfirmEmail = LocalContext.current.getSharedPreferences(
+                    "authorization", MODE_PRIVATE
+                ).getBoolean("isConfirmEmail", false)
+
+                val startDestination = if (token != "") {
+                    Screens.SignIn.route
+                } else {
+                    if (isConfirmEmail) {
+                        Screens.Main.route
+                    } else {
+                        Screens.ConfirmEmail.route
+                    }
+                }
+
+                val navController = rememberNavController()
+                AppNavHost(navController = navController, startDestination = startDestination)
             }
         }
     }
